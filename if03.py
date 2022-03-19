694a20b21ba26e8d4f2f230f129e1eb328a3ff39
oybekEsirgabov <90684441+tillabola@users.noreply.github.com>
2022-03-19 13:40:08 +0500

    3

--- if03.py
+++ if03.py
@@ -6,4 +6,13 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if (a > 0): 
+        return a+1
+    if (a == 0):
+        return 10
+    if (a < 0):
+        return a
+
+
+    #return
+print(main(1))
