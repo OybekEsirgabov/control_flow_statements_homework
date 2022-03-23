98580bfc542ea55c081a80fe6600ea09228d09e2
oybekEsirgabov <90684441+tillabola@users.noreply.github.com>
2022-03-23 13:35:41 +0500

    08

--- if08.py
+++ if08.py
@@ -11,4 +11,12 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a%2 > 0:
+        x1 = a/100
+    else: 
+        x2 = a/100
+    if (x1 > 1):
+        return "two-digit odd number"
+    if (x2 > 1):
+        return "two-digit even number"
+print(main(408))
